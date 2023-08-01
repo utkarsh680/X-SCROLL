44be0ea1291d0c0c408c9c5f7092a25416030566
Utkarsh Singh <singhrishu680@gmail.com>
2023-08-01 20:22:46 +0530

    Some Css change

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ slider.addEventListener('mousemove', (e) => {
   const x = e.pageX - slider.offsetLeft;
   const walk = (x - startX) * 3; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
+
   console.log(walk);
 });
 
